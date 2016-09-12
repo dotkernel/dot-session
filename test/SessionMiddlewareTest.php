42a6b01c5357fd5101fde25ae87ff81bc0f82b74
n3vrax <n3vrax@gmail.com>
2016-09-12 21:21:41 +0300

    file comments update

--- test/SessionMiddlewareTest.php
+++ test/SessionMiddlewareTest.php
@@ -1,14 +1,19 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: n3vrax
- * Date: 9/9/2016
- * Time: 8:01 PM
+ * @copyright: DotKernel
+ * @library: dotkernel/dot-session
+ * @author: n3vrax
+ * Date: 9/5/2016
+ * Time: 8:24 PM
  */
 
 namespace DotKernelTest\DotSession;
 
 
+/**
+ * Class SessionMiddlewareTest
+ * @package DotKernelTest\DotSession
+ */
 class SessionMiddlewareTest extends \PHPUnit_Framework_TestCase
 {
     public function testSomething()
